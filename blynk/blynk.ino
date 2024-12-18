5e9d37adcf4dc1fdf6731636c1ab7daf27afd1af
logan-hartford <91397886+logan-hartford@users.noreply.github.com>
2024-12-18 13:49:40 -0500

    kinda works

--- blynk/blynk.ino
+++ blynk/blynk.ino
@@ -13,6 +13,7 @@ char pass[] = "logan123";
 
 enum Mode {
   MANUAL,
+  CYCLE,
   DISCO,
   CANDLE,
 };
@@ -21,19 +22,21 @@ enum Mode {
 #define LED_PIN 2
 #define NUM_LEDS 16
 
-Adafruit_NeoPixel ring(NUM_LEDS, LED_PIN, NEO_RGBW + NEO_KHZ800);
+Adafruit_NeoPixel ring(NUM_LEDS, LED_PIN, NEO_RGB + NEO_KHZ800);
 
 int mode = 0;
-int brightness = 0;
-int speed = 0;
-int red = 0;
-int green = 0;
-int blue = 0;
+uint8_t brightness = 0;
+int speed = 5;
+uint8_t red = 0;
+uint8_t green = 0;
+uint8_t blue = 0;
+int colorIndex = 0;
 
 // Brightness
 BLYNK_WRITE(V0)
 {
-  brightness = param.asInt(); 
+  brightness = (uint8_t)param.asInt();
+  brightness = brightness * 10;
 
   Serial.print("Brightness is: ");
   Serial.println(brightness);
@@ -42,7 +45,7 @@ BLYNK_WRITE(V0)
 // Mode
 BLYNK_WRITE(V1)
 {
-  mode = param.asInt(); 
+  mode = param.asInt() - 1; 
 
   Serial.print("Mode is: ");
   Serial.println(mode);
@@ -51,7 +54,7 @@ BLYNK_WRITE(V1)
 // Speed
 BLYNK_WRITE(V3)
 {
-  speed = param.asInt(); 
+  speed = 10 - param.asInt() + 1; 
 
   Serial.print("Speed is: ");
   Serial.println(speed);
@@ -60,7 +63,7 @@ BLYNK_WRITE(V3)
 // Red
 BLYNK_WRITE(V4)
 {
-  red = param.asInt(); 
+  red = (uint8_t)param.asInt(); 
 
   Serial.print("Red is: ");
   Serial.print(red);
@@ -69,7 +72,7 @@ BLYNK_WRITE(V4)
 // Green
 BLYNK_WRITE(V5)
 {
-  green = param.asInt(); 
+  green = (uint8_t)param.asInt(); 
 
   Serial.print(" Green is: ");
   Serial.print(green);
@@ -78,11 +81,60 @@ BLYNK_WRITE(V5)
 // Blue
 BLYNK_WRITE(V6)
 {
-  blue = param.asInt(); 
+  blue = (uint8_t)param.asInt(); 
 
   Serial.print(" Blue is: ");
   Serial.println(blue);
 }
+
+void updateRingColor() {
+  switch (mode) {
+    case MANUAL:
+      // Static manual color
+      for (int i = 0; i < ring.numPixels(); i++) {
+        ring.setPixelColor(i, ring.Color(green, red, blue));
+      }
+      break;
+
+    case DISCO:
+      // Disco mode: Rainbow animation (each LED different)
+      for (int i = 0; i < ring.numPixels(); i++) {
+        ring.setPixelColor(i, ring.ColorHSV((colorIndex + (i * 65536 / NUM_LEDS)) % 65536, 255, brightness));
+      }
+      colorIndex = (colorIndex + 256) % 65536; // Increment hue
+      break;
+
+    case CANDLE: {
+      static float smoothBrightness = brightness; 
+      float target = brightness - 20 + random(40); 
+      if (target < 0) target = 0;
+      if (target > 255) target = 255;
+
+      smoothBrightness = smoothBrightness * 0.9 + target * 0.1;
+
+      for (int i = 0; i < ring.numPixels(); i++) {
+        ring.setPixelColor(i, 95, 180, 0);
+      }
+      ring.setBrightness((uint8_t)smoothBrightness);
+      break;
+    }
+
+    case CYCLE:
+      // Cycle mode: All LEDs same color, cycling through RGB spectrum
+      uint32_t cycleColor = ring.ColorHSV(colorIndex, 255, brightness);
+      for (int i = 0; i < ring.numPixels(); i++) {
+        ring.setPixelColor(i, cycleColor);
+      }
+      colorIndex = (colorIndex + 256) % 65536; // Increment hue
+      break;
+  }
+  if (mode != CANDLE)
+  {
+    ring.setBrightness(brightness);
+  }
+  ring.show();
+}
+
 
 
 void setup() {
@@ -102,10 +154,27 @@ void setup() {
 
   ring.begin();
   ring.show();
-  ring.setBrightness(50);
+
+  Blynk.syncAll();
 }
 
 void loop() {
   Blynk.run();
+  updateRingColor();
+  if (mode == CANDLE)
+  {
+    if (random(10)==0)
+    {
+      delay(random(60, 100));
+    }
+    else
+    {
+      delay(random(120,200));
+    }
+  }
+  else
+  {
+    delay(5*speed);
+  }
   yield();
 }
